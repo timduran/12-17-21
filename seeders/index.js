b73f5f49a7c4d390aa0be5c71a6d50bef5e7fe24
Quinton Fults <quinton.fults.work@gmail.com>
2021-12-17 13:52:16 -0800

    code

--- seeders/index.js
+++ seeders/index.js
@@ -3,7 +3,10 @@ const { User, Post } = require('../models')
 async function seeder() {
   console.log('----Seeding Data----')
 
-  await User.bulkCreate(require('./userSeed.js'))
+  await User.register(new User({ username: 'johndoe', email: 'johndoe@gmail.com' }), 'password1234')
+  await User.register(new User({ username: 'janedoe', email: 'janedoe@gmail.com' }), 'password4321')
+  await User.register(new User({ username: 'jackdoe', email: 'jackdoe@gmail.com' }), 'rootroot')
+
   await Post.bulkCreate(require('./postSeed.js'))
 
   console.log('----Data Seeded----')
